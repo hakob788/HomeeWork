a3789c8108202d8d91562619a5532b0f5c3e3e64
Hakob <hakobhakobyan389@gmail.com>
2023-02-11 01:28:53 +0400

    Commit

---
+++ src/homework/homework10/EmployeeDemo.java
@@ -0,0 +1,57 @@
+package homework.homework10;
+
+import java.util.Scanner;
+
+public class EmployeeDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        EmployeeStorage es = new EmployeeStorage();
+
+        boolean isRun = true;
+        while (isRun) {
+            System.out.println("0 for exit");
+            System.out.println("1 add employee");
+            System.out.println("2 print all employee");
+            System.out.println("3 search employee by employee ID");
+            System.out.println("4 search employee by company name");
+
+            String command = scanner.nextLine();
+            switch (command) {
+                case "0":
+                    isRun = false;
+                    break;
+                case "1":
+                    System.out.println("Please input name");
+                    String name = scanner.nextLine();
+                    System.out.println("Please input surname");
+                    String surname = scanner.nextLine();
+                    System.out.println("Please input employeeID");
+                    String employeeID = scanner.nextLine();
+                    System.out.println("Please input salary");
+                    String salaryStr = scanner.nextLine();
+                    System.out.println("Please input company ");
+                    String company = scanner.nextLine();
+                    System.out.println("Please input position");
+                    String position = scanner.nextLine();
+                    double salary = Double.parseDouble(salaryStr);
+                    Employee employee = new Employee(name, surname, employeeID, salary, company, position);
+                    es.add(employee);
+                    System.out.println("employee was added");
+                    break;
+                case "2":
+                    es.print();
+                    break;
+                case "3":
+                    System.out.println("Please input employeeID");
+                    employeeID = scanner.nextLine();
+                    es.searchByEmployeeID(employeeID);
+                    break;
+                case "4":
+                    System.out.println("Please input company");
+                    String companyName = scanner.nextLine();
+                    es.searchByCompany(companyName);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/homework/homework10/EmployeeStorage.java
@@ -0,0 +1,54 @@
+package homework.homework10;
+
+public class EmployeeStorage {
+    private Employee[] array = new Employee[10];
+    private int size = 0;
+
+    public void add(Employee value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    private void extend() {
+        Employee[] newArray = new Employee[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+    public void searchByEmployeeID(String employeeID) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            Employee employee = array[i];
+            if (employee.getEmployeeID(). equals(employeeID)){
+                found = true;
+                System.out.println(employee);
+            }
+        }
+        if (!found) {
+            System.out.println("There is no employee with this ID");
+        }
+    }
+    public void searchByCompany(String companyName) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            Employee employee = array[i];
+            if (employee.getCompany().startsWith(companyName)) {
+                found = true;
+                System.out.println(employee);
+            }
+        }
+        if (!found) {
+            System.out.println("No such company exists");
+        }
+    }
+}
