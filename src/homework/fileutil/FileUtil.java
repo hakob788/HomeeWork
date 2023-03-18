c226a8b196451987ac02fd343e30e814a7f934d4
Hakob <hakobhakobyan389@gmail.com>
2023-03-18 21:30:22 +0400

    Commit

--- src/homework/fileutil/FileUtil.java
+++ src/homework/fileutil/FileUtil.java
@@ -9,7 +9,10 @@ public class FileUtil {
 
     public static void main(String[] args) {
         //System.out.println(fileSearch());
-        contentSearch();
+        //contentSearch();
+        //findLines();
+        //printSizeOfPackage();
+        createFileWithContent();
     }
 
     //այս մեթոդը պետք է սքաններով վերցնի երկու string.
@@ -38,7 +41,6 @@ public class FileUtil {
         String keyword = scanner.nextLine();
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
-
         for (File file : files) {
             if (file.isFile() && file.getName().contains(".txt")) {
                 try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -47,12 +49,12 @@ public class FileUtil {
                         if (line.contains(keyword)) {
                             System.out.println(file.getName());
                             break;
-                        }else {
-                            System.out.println("null");
+                        } else {
+                            System.out.println("wrong");
                         }
                     }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.out.println("null");
                 }
             }
         }
@@ -65,14 +67,35 @@ public class FileUtil {
     static void findLines() {
         String txtPath = scanner.nextLine();
         String keyword = scanner.nextLine();
-
+        try (BufferedReader br = new BufferedReader(new FileReader(txtPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(keyword)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("null");
+        }
     }
 
     //այս մեթոդը պետք է սքաններով վերցնի մեկ string.
     // 1 - path թե որ ֆոլդերի չափն ենք ուզում հաշվել
     // ֆոլդերի բոլոր ֆայլերի չափսերը գումարում ենք իրար, ու տպում
     static void printSizeOfPackage() {
-
+        String path = scanner.nextLine();
+        File directory = new File(path);
+        if (directory.isDirectory()) {
+            int size = 0;
+            File file = new File(path);
+            File[] files = directory.listFiles();
+            for (File file1 : files) {
+                if (file1.isFile()) {
+                    size += file1.length();
+                }
+            }
+            System.out.println(size);
+        }
     }
 
     //այս մեթոդը պետք է սքաններով վերցնի երեք string.
@@ -81,5 +104,18 @@ public class FileUtil {
     // 3 - content ֆայլի պարունակությունը։ Այսինքն ստեղծված ֆայլի մեջ ինչ է գրելու
     // որպես արդյունք պապկի մեջ սարքելու է նոր ֆայլ, իրա մեջ էլ լինելու է content-ով տվածը
     static void createFileWithContent() {
+        String path = scanner.nextLine();
+        String fileName = scanner.nextLine();
+        String content = scanner.nextLine();
+        String filePath = path + File.separator + fileName;
+        File newFile = new File(filePath);
+        try {
+            FileWriter writer = new FileWriter(newFile);
+            writer.write(content);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("null");
+        }
     }
 }
+
